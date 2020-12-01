b68814030d4702285cb4e941b950dd0483408569
KonstantinPitersky <piterskyff@gmail.com>
2020-12-01 18:14:09 +0300

    first commit

---
+++ #source/js/script.js
@@ -0,0 +1,29 @@
+'use strict'
+
+function start() {
+	document.querySelector('.main__row').classList.add('scroll-up');
+	document.removeEventListener("click", start);
+	new Promise((resolve) => {
+		setTimeout(()=> {
+		document.querySelector('.main__row').classList.add('resume-reading');
+		document.querySelector('.main__label').classList.add('show-bg');
+		resolve();
+		}, 1000)
+	}).then(() => {
+		setTimeout(() => {
+			document.querySelector('.label__photo').classList.add('scale-item');
+			document.querySelector('.contacts__title').classList.add('show-item');
+			}, 1000)
+	}).then(() => {
+		setTimeout(() => {
+			document.querySelector('.contacts__ul').classList.add('show-contacts');
+		}, 2000)
+		
+	}).then(() => {
+		setTimeout(()=> {
+			document.body.classList.remove('no-scroll');
+		}, 3000)
+	})
+}
+
+document.addEventListener("click", start);
