1dd9461be4e22e1ae08a5bf1fcab038ee1de9098
KonstantinPitersky <piterskyff@gmail.com>
2020-12-02 13:37:01 +0300

    adaptive add

--- js/main.js
+++ js/main.js
@@ -1,30 +1,40 @@
 
 'use strict'
-
-function start() {
-	document.querySelector('.main__row').classList.add('scroll-up');
-	document.removeEventListener("click", start);
-	new Promise((resolve) => {
-		setTimeout(()=> {
-		document.querySelector('.main__row').classList.add('resume-reading');
-		document.querySelector('.main__label').classList.add('show-bg');
-		resolve();
-		}, 1000)
-	}).then(() => {
-		setTimeout(() => {
-			document.querySelector('.label__photo').classList.add('scale-item');
-			document.querySelector('.contacts__title').classList.add('show-item');
+class Main {
+	showResume() {
+		document.removeEventListener("click", this.showResume);
+		document.querySelector('.main__row').classList.add('scroll-up');
+		new Promise((resolve) => {
+			setTimeout(()=> {
+			document.querySelector('.burger__body').classList.add('show-item');
+			document.querySelector('.main__row').classList.add('resume-reading');
+			document.querySelector('.main__label').classList.add('show-bg');
+			resolve();
 			}, 1000)
-	}).then(() => {
-		setTimeout(() => {
-			document.querySelector('.contacts__ul').classList.add('show-contacts');
-		}, 2000)
-		
-	}).then(() => {
-		setTimeout(()=> {
-			document.body.classList.remove('no-scroll');
-		}, 3000)
-	})
+		}).then(() => {
+			setTimeout(() => {
+				document.querySelector('.label__photo').classList.add('scale-item');
+				document.querySelector('.contacts__title').classList.add('show-item');
+				}, 1000)
+		}).then(() => {
+			setTimeout(() => {
+				document.querySelector('.contacts__ul').classList.add('show-contacts');
+			}, 2000)
+			
+		}).then(() => {
+			setTimeout(()=> {
+				document.body.classList.remove('no-scroll');
+			}, 3000)
+		})
+	}
+	toggleInformation() {
+		document.querySelector('.burger__body').classList.toggle('active-burger',);
+		document.querySelector('.main__label').classList.toggle('active-menu');
+	}
+
 }
 
-document.addEventListener("click", start);
+let main = new Main;
+
+document.addEventListener("click", main.showResume);
+document.querySelector('.burger__body').addEventListener('click', main.toggleInformation);
